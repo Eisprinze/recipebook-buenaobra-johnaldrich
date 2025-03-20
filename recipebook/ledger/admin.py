762f9654314107bba5bb04e4b615ca9c11048af1
drichie <defectective@gmail.com>
2025-03-21 04:26:21 +0800

    third lab branch contains login logout features

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Ingredients,Recipe,RecipeIngredient
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import *
+
 # Register your models here.
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = [ProfileInline,]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Ingredients)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,29 +1,45 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredients(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         reverse('Ingredients', args=[str(self.name)])
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.id)])  
-    
+        return reverse('recipe_detail', args=[str(self.id)])
+
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredients = models.ForeignKey(Ingredients,on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe,on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{}: {} , {} '.format(self.ingredients,self.quantity,self.recipe)
+        return '{}:{},{}'.format(self.ingredients, self.quantity, self.recipe)
